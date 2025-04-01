18d7728da8489562d8f09443c83b011807576d50
Rinc <i@rinc.xyz>
2025-04-01 10:54:06 +0800

    hash & base64;

--- src/crypto.rs
+++ src/crypto.rs
@@ -1,7 +1,28 @@
+use hex;
 use aes::Aes256;
 use block_modes::{BlockMode, Ecb};
 use block_modes::block_padding::Pkcs7;
+use md5::{Md5, Digest};
+use sha1::{Sha1, Digest};
+use sha2::{Sha256, Digest};
+use base64::{Engine as _, engine::general_purpose};
 use std::error::Error;
+
+pub fn str2bytes(s: String) -> Vec<u8> {
+    s.into_bytes()
+}
+
+pub fn bytes2str(v: Vec<u8>) -> Result<String, Box<dyn Error>> {
+    String::from_utf8(v).map_err(|e| e.into())
+}
+
+pub fn hex2bytes(hex: &str) -> Result<Vec<u8>, Box<dyn Error>> {
+    hex::decode(hex).map_err(|e| e.into())
+}
+
+pub fn bytes2hex(bytes: &[u8]) -> String {
+    hex::encode(bytes)
+}
 
 type Aes256Ecb = Ecb<Aes256, Pkcs7>;
 
@@ -26,4 +47,31 @@ impl Aes256EcbPkcs5 {
     pub fn decrypt(&self, data: &[u8]) -> Result<Vec<u8>, Box<dyn Error>> {
         Ok(self.cipher.decrypt_vec(data)?)
     }
+}
+
+pub fn hash_md5(data: &[u8]) -> Vec<u8> {
+    let mut hasher = Md5::new();
+    hasher.update(data);
+    hasher.finalize().to_vec()
+}
+
+pub fn hash_sha1(data: &[u8]) -> Vec<u8> {
+    let mut hasher = Sha1::new();
+    hasher.update(data);
+    hasher.finalize().to_vec()
+}
+
+pub fn hash_sha256(data: &[u8]) -> Vec<u8> {
+    let mut hasher = Sha256::new();
+    hasher.update(data);
+    hasher.finalize().to_vec()
+}
+
+pub fn base64_encode(data: &[u8]) -> Vec<u8> {
+    general_purpose::STANDARD.encode(data).into_bytes()
+}
+
+pub fn base64_decode(data: &[u8]) -> Result<Vec<u8>, Box<dyn Error>> {
+    let s = std::str::from_utf8(data)?;
+    general_purpose::STANDARD.decode(s).map_err(|e| e.into())
 }
--- src/crypto_c.rs
+++ src/crypto_c.rs
@@ -1,6 +1,6 @@
 use std::os::raw::c_char;
 use crate::cc::{cbytes_to_rust, rust_to_cbytes, ngenrs_free_ptr};
-use crate::crypto::Aes256EcbPkcs5;
+use crate::crypto::{Aes256EcbPkcs5, hash_md5, hash_sha1, hash_sha256, base64_encode, base64_decode};
 
 #[unsafe(no_mangle)]
 pub extern "C" 
@@ -66,3 +66,69 @@ pub extern "C"
 fn ngenrs_crypto_aes256_ecb_pkcs5_release(cipher: *mut Aes256EcbPkcs5) {
     ngenrs_free_ptr(cipher);
 }
+
+#[unsafe(no_mangle)]
+pub extern "C" 
+fn ngenrs_crypto_hash_md5(data: *const u8, data_len: usize, out_len: *mut usize) -> *mut u8 {
+    if data.is_null() {
+        return std::ptr::null_mut();
+    }
+    let data_bytes = unsafe { cbytes_to_rust(data, data_len) };
+    let hash = hash_md5(data_bytes);
+    let (ptr, len) = rust_to_cbytes(hash.as_bytes());
+    unsafe { *out_len = len };
+    ptr
+}
+
+#[unsafe(no_mangle)]
+pub extern "C"
+fn ngenrs_crypto_hash_sha1(data: *const u8, data_len: usize, out_len: *mut usize) -> *mut u8 {
+    if data.is_null() {
+        return std::ptr::null_mut();
+    }
+    let data_bytes = unsafe { cbytes_to_rust(data, data_len) };
+    let hash = hash_sha1(data_bytes);
+    let (ptr, len) = rust_to_cbytes(hash.as_bytes());
+    unsafe { *out_len = len };
+    ptr
+}
+
+#[unsafe(no_mangle)]
+pub extern "C"
+fn ngenrs_crypto_hash_sha256(data: *const u8, data_len: usize, out_len: *mut usize) -> *mut u8 {
+    if data.is_null() {
+        return std::ptr::null_mut();
+    }
+    let data_bytes = unsafe { cbytes_to_rust(data, data_len) };
+    let hash = hash_sha256(data_bytes);
+    let (ptr, len) = rust_to_cbytes(hash.as_bytes());
+    unsafe { *out_len = len };
+    ptr
+}
+
+#[unsafe(no_mangle)]
+pub extern "C"
+fn ngenrs_crypto_base64_encode(data: *const u8, data_len: usize, out_len: *mut usize) -> *mut u8 {
+    if data.is_null() {
+        return std::ptr::null_mut();
+    }
+    let data_bytes = unsafe { cbytes_to_rust(data, data_len) };
+    let encoded = base64_encode(data_bytes);
+    let (ptr, len) = rust_to_cbytes(encoded.as_bytes());
+    unsafe { *out_len = len };
+    ptr
+}
+
+#[unsafe(no_mangle)]
+pub extern "C"
+fn ngenrs_crypto_base64_decode(data: *const u8, data_len: usize, out_len: *mut usize) -> *mut u8 {
+    if data.is_null() {
+        return std::ptr::null_mut();
+    }
+    let data_bytes = unsafe { cbytes_to_rust(data, data_len) };
+    let decoded = base64_decode(data_bytes);
+    let (ptr, len) = rust_to_cbytes(decoded.as_bytes());
+    unsafe { *out_len = len };
+    ptr
+}
+
