7eca9760378d403aae7d50e3163ccdbd54a40b7a
Rinc <i@rinc.xyz>
2025-04-03 12:03:30 +0800

    Renma mod files for C;

--- src/lib.rs
+++ src/lib.rs
@@ -9,10 +9,10 @@ pub mod core {
 
 pub mod c {
     pub mod util;
-    pub mod crypto_c;
-    pub mod db_c;
-    pub mod kv_c;
-    pub mod net_c;
-    pub mod zip_c;
-    pub mod lua_c;
+    pub mod crypto;
+    pub mod db;
+    pub mod kv;
+    pub mod net;
+    pub mod zip;
+    pub mod lua;
 }
